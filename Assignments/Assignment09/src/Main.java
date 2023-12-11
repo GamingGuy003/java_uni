3da7023e494b5ed8cba3c19e407c957abdda22f4
GamingGuy003 <bjornfelixh@gmail.com>
2023-12-11 22:17:54 +0100

    continued work

--- Assignments/Assignment09/src/Main.java
+++ Assignments/Assignment09/src/Main.java
@@ -4,40 +4,67 @@ import static java.lang.Integer.parseInt;
 
 public class Main {
     public static void main(String[] args) {
-        MultiplicationTable mt = new MultiplicationTable(inputNumber("Input n: "), inputNumber("Input m: "));
-
-        int[][] multTable = new int[n][m];
-
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                System.out.print(multTable[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    static int inputNumber(String message) {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.print(message);
-            try {
-                return Integer.parseInt(sc.nextLine());
-            } catch (Exception e) {
-                System.out.print("Not a number, try again");
-            }
-        }
+        MultiplicationTable mt = new MultiplicationTable();
+        mt.run();
     }
 }
 
 class MultiplicationTable {
     int[][] table;
-    MultiplicationTable(int n, int m) {
+    int n, m;
+    MultiplicationTable() {
+        n = inputNumber("Input n: ");
+        m = inputNumber("Input m: ");
         this.table = new int[n][m];
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 this.table[i][j] = (j + 1) * (i + 1);
             }
         }
     }
+
+    void printTable() {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                System.out.print(table[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void run() {
+        while (true) {
+            printTable();
+            int x = inputNumber("row " + "[1.." + table.length + "] (exit to quit): ") - 1;
+            int y = inputNumber("column" + "[1.." + table[0].length + "]  (exit to quit): ") - 1;
+            try {
+                table[x][y] = inputNumber("Value at (" + (x + 1) + "," + (y + 1) + ") is " + table[x][y] + ". Replace with (exit to quit): ");
+            } catch (Exception e) {
+                System.out.println("Input out of bounds, try again");
+            }
+        }
+    }
+
+    int inputNumber(String message) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print(message);
+            try {
+                return exit(sc.nextLine());
+            } catch (Exception e) {
+                System.out.print("Not a number, try again");
+            }
+        }
+    }
+
+    int exit(String input) throws Exception {
+        if (input.equals("exit")) {
+            System.out.println("Exiting...");
+            System.exit(0);
+        }
+
+        return Integer.parseInt(input);
+    }
+
 }
